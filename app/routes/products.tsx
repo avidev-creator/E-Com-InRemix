f273776fc191647dc1df79ed16d11db76ace7d33
avidev-creator <avidub371@gmail.com>
2025-02-19 20:40:36 +0530

    testing pr changes

--- app/routes/products.tsx
+++ app/routes/products.tsx
@@ -1,5 +1,5 @@
 import { FC } from "react";
-import Card from "../components/Card";
+import Card from "../../components/Card";
 
 const Index: FC = () => {
   const cards = [
---
+++ app/routes/testing.tsx
@@ -0,0 +1,7 @@
+export default function TestingPage(){
+  return (
+    <div className = "container mx-auto ">
+      <h1 className= "text-4xl">Testing</h1>
+    </div>
+  )
+}
